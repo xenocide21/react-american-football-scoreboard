f45ac5db06fa80ef0c5ee6431ec87fedb5710679
Ashley Dunham <xenocide21@gmail.com>
2019-11-04 14:11:06 -0700

    MVP done, down, yards to go, ball on and quarter have buttons and work. Buttons are not with the other buttons (couldn't figure that out, dont care)

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,27 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-const BottomRow = () => {
-  return (
-    <div className="bottomRow">
-      <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
-      </div>
-      <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
-      </div>
-      <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
-      </div>
-      <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
-      </div>
-    </div>
-  );
-};
+function BottomRow() {
+    const [down, setDown] = useState(1);
+    const [yards, setYards] = useState(10);
+    const [ballOn, setBallOn] = useState(20);
+    const [quarter, setQuarter] = useState(1);
 
+    // const BottomRow = () => {
+        return (
+            <div>
+                <div className="bottomRow">
+                    <div className="down">
+                        <h3 className="down__title">Down</h3>
+                        <div className="down__value">{down}</div>
+                    </div>
+                    <div className="toGo">
+                        <h3 className="toGo__title">To Go</h3>
+                        <div className="toGo__value">{yards}</div>
+                    </div>
+                    <div className="ballOn">
+                        <h3 className="ballOn__title">Ball on</h3>
+                        <div className="ballOn__value">{ballOn}</div>
+                    </div>
+                    <div className="quarter">
+                        <h3 className="quarter__title">Quarter</h3>
+                        <div className="quarter__value">{quarter}</div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h5>Down</h5>
+                        <button className="downButton_counter" onClick={()=>setDown(down +1)}>Add</button>
+                        <button className="downButton_counter" onClick={()=>setDown(1)}>Reset</button>
+                    </div>
+                    <div>
+                        <h5>Yards</h5>
+                        <button className="yardsToGo" onClick={()=>setYards(yards -1)}>-1</button>
+                        <button className="yardsToGo" onClick={()=>setYards(yards -5)}>-5</button>
+                        <button className="yardsToGo" onClick={()=>setYards(yards +1)}>+1</button>
+                        <button className="yardsToGo" onClick={()=>setYards(yards +5)}>+5</button>
+                        <button className="yardsToGo" onClick={()=>setYards(10)}>Reset</button>
+                    </div>
+                    <div>
+                        <h5>Ball On</h5>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn -1)}>-1</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn -5)}>-5</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn -10)}>-10</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn +1)}>+1</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn +5)}>+5</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(ballOn +10)}>+10</button>
+                        <button className="ballOnYard" onClick={()=>setBallOn(20)}>Reset</button>
+                    </div>
+                    <div>
+                        <h5>Quarter</h5>
+                        <button className="quarterTime" onClick={()=>setQuarter(quarter +1)}>Add</button>
+                        <button className="quarterTime" onClick={()=>setQuarter(1)}>Reset</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 export default BottomRow;
